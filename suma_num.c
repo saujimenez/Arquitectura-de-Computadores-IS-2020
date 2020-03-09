1c5e4dceacee456a2ae6e785511e360f623913d1
saujimenez <61956489+saujimenez@users.noreply.github.com>
2020-03-08 21:31:18 -0600

    Archivos de la Tarea de Arqui
    Programa que suma dos digitos y el archivo binario de IDA Pro

---
+++ suma_num.c
@@ -0,0 +1,14 @@
+//Bibliotecas necesarias
+#include <stdio.h>
+
+//Funcion principal del programa
+int main() {
+	sumaNum(2,10);
+	return 0;
+	}
+	
+void sumaNum(int num1,int num2){
+	//Funcion para sumar los numeros
+	int suma = num1 + num2;
+	printf("La suma es igual a: %d", suma);
+	}
